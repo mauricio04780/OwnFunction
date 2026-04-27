b19a23774d17df9fa18ce34d7645376e7bbb7b30
matbing1 <gmatias2641@gmail.com>
2026-04-27 18:41:43 -0500

    f(x)

--- R/down.R
+++ R/down.R
@@ -1,12 +1,12 @@
-#' Probability density function of the OwnFunction distribution
+#' Function associated with the OwnFunction distribution
 #'
-#' @param x A numeric value or vector where the PDF is evaluated
+#' @param x A numeric value or vector where the function is evaluated
 #'
-#' @return Returns the value of the probability density function at x
+#' @return Returns the value of the function at x
 #'
 #' @details
-#' This function computes the probability density function (PDF) associated
-#' with the custom continuous distribution defined in the package. The function is:
+#' This function evaluates a piecewise-defined function on the interval (-1, 2).
+#' It is given by:
 #' \deqn{
 #' f(x) = \begin{cases}
 #' \frac{x^2}{3} & -1 < x < 2 \\
@@ -15,8 +15,8 @@
 #' }
 #'
 #' @examples
-#' down(0)
-#' down(c(-2, -0.5, 0, 0.5, 1.5, 2.5))
+#' down(1)
+#' down(c(-0.5, 0.5, 1.5))
 #' @export
 down <- function(x){
   ifelse(x > -1 & x < 2, x^2/3, 0)
